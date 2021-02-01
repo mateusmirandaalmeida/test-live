4095b23f129047ba28f21d7273247831d369dc53
Mateus <info.mateusmiranda@gmail.com>
2021-02-01 18:59:12 -0300

    merge

--- public/room.js
+++ public/room.js
@@ -1,8 +1,8 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
-  host: "",
-  port: "",
+  host: "miramiraaa.pagekite.me",
+  port: "80",
 });
 const myVideo = document.createElement("video");
 myVideo.muted = true;
